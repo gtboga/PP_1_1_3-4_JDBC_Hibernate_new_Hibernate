df08fbc3406b95a15800d4b9bbe29ecf0c8ce52b
gtboga <122990173+gtboga@users.noreply.github.com>
2024-03-10 21:16:16 +0300

    Первый commit. JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserDao userDaoJDBC = new UserDaoJDBCImpl();
+
+        userDaoJDBC.createUsersTable();
+
+        userDaoJDBC.saveUser("Name1", "LastName1", (byte) 1);
+        userDaoJDBC.saveUser("Name2", "LastName2", (byte) 2);
+        userDaoJDBC.saveUser("Name3", "LastName3", (byte) 3);
+        userDaoJDBC.saveUser("Name4", "LastName4", (byte) 4);
+
+        userDaoJDBC.removeUserById(1);
+        userDaoJDBC.getAllUsers();
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,83 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
-
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+                    "name VARCHAR(45)," + "lastname VARCHAR(45)," + "age INT(3))");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute("DROP TABLE IF EXISTS users");
+        }   catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Connection connection = Util.getConnection()) {
+            PreparedStatement ps = connection
+                    .prepareStatement("INSERT INTO users(name, lastName, age) VALUES (?, ?, ?)");
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setInt(3, age);
+            ps.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Connection connection = getConnection();
+             PreparedStatement ps = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
+            ps.setLong(1, id);
+            ps.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> resultList = new ArrayList<>();
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery("SELECT * FROM users");
+            while (rs.next()) {
+                User user = new User(rs.getString("name"), rs.getString("lastname"), rs.getByte("age"));
+                user.setId(rs.getLong("id"));
+                resultList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultList;
+
     }
 
     public void cleanUsersTable() {
-
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+            statement.execute( "DELETE FROM users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
